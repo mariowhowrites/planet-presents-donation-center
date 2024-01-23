8875dff7aa051857723e477fbe4e73840528e696
Mario Vega <mariovegadev@gmail.com>
2024-01-23 14:58:23 -0800

    initial planet-presents commit

---
+++ app/Filament/Resources/CharityResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CharityResource\Pages;
+use App\Filament\Resources\CharityResource\RelationManagers;
+use App\Filament\Resources\CharityResource\RelationManagers\TiersRelationManager;
+use App\Models\Charity;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CharityResource extends Resource
+{
+    protected static ?string $model = Charity::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->unique(ignorable: fn ($record) => $record)
+                    ->maxLength(255),
+                Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+                TextInput::make('donation_url')
+                    ->required()
+                    ->label('Donation URL')
+                    ->helperText('This is the URL that will be used to make donations to this charity.')
+                    ->url()
+                    ->maxLength(255),
+                TextInput::make('charity_url')
+                    ->required()
+                    ->label('Charity URL')
+                    ->helperText('This is the URL that will be used to link to this charity.')
+                    ->url()
+                    ->maxLength(255),
+                FileUpload::make('preview_image')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            TiersRelationManager::class
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCharities::route('/'),
+            'create' => Pages\CreateCharity::route('/create'),
+            'edit' => Pages\EditCharity::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/CharityResource/RelationManagers/TiersRelationManager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Resources\CharityResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TiersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'tiers';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('amount')
+                    ->required()
+                    ->label('Donation Amount')
+                    ->numeric(),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('amount'),
+                Tables\Columns\TextColumn::make('description'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
